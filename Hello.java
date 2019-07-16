3a1f38718cb7f88e6879f15508b24fe6eab5e9ed
salagarsprabu <37106724+salagarsprabu@users.noreply.github.com>
2019-07-16 20:48:11 +0530

    added detailed message

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello{
 	public static void main(String args[]){
 		for (int i=1;i<=10;i++){
 			System.out.println("Hello World....."+i);
-			System.out.println("Thanks..");
+			System.out.println("Thanks for the webhooks demo..");
 		}
 	}
 	
