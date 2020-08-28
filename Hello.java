214ed6e727513cb83f9d3a0a14470c9229988cac
salagarsprabu <37106724+salagarsprabu@users.noreply.github.com>
2020-08-28 15:30:34 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello{
 		for (int i=1;i<=10;i++){
 			System.out.println("Hello World....."+i);
 			System.out.println("Thanks for the webhooks demo..");
-			System.out.println("Thanks for the webhooks demo1..");			
+			System.out.println("Thanks for the webhooks demo2..");			
 			System.out.println("demo on webhooks - 28Aug 2020");
 		}
 	}
