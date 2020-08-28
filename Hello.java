20daa3f5fb22667de60092ea84737287f1dd5f6a
salagarsprabu <37106724+salagarsprabu@users.noreply.github.com>
2020-08-28 14:37:46 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,23 +4,8 @@ public class Hello{
 		for (int i=1;i<=10;i++){
 			System.out.println("Hello World....."+i);
 			System.out.println("Thanks for the webhooks demo..");
-			System.out.println("Thanks for the webhooks demo1..");
-			System.out.println("Thanks for the webhooks demonstration..");
-			System.out.println("Thanks for the webhooks demonstration 2..");
-			System.out.println("Thanks for the webhooks demonstration 3..");
-			System.out.println("Thanks for the webhooks demonstration 4..");
-			System.out.println("Thanks for the webhooks demonstration 5..");
-			System.out.println("Thanks for the webhooks demonstration 6..");
-			System.out.println("Thanks for the webhooks demonstration 8..");
-			System.out.println("Thanks for the webhooks demonstration 9..");
-			System.out.println("Thanks for the webhooks demonstration 10..");
-			System.out.println("Thanks for the webhooks demonstration 11..");
-			System.out.println("Thanks for the webhooks demonstration 13..");
-			System.out.println("Thanks for the webhooks demonstration 1444666..");
-			System.out.println("Thanks for the webhooks demonstration test..");
-			System.out.println("Thanks for the webhooks demonstration on 17-feb-2020..");
-			System.out.println("Thanks for the webhooks demonstration on today..");
-			System.out.println("demo on webhooks - july24 2020");
+			System.out.println("Thanks for the webhooks demo1..");			
+			System.out.println("demo on webhooks - Aug 2020");
 		}
 	}
 	
