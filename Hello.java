8fb4d46e92b8752345849ac8e45718784f075d3f
salagarsprabu <37106724+salagarsprabu@users.noreply.github.com>
2022-01-27 17:22:12 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -5,18 +5,7 @@ public class Hello{
 			System.out.println("Hello World....."+i);
 			System.out.println("Thanks for the webhooks demo..");
 			System.out.println("Thanks for the webhooks demo2..");			
-			System.out.println("demo on webhooks - 04sep 2020");
-			System.out.println("demo on webhooks - Nov 25 2020- demo");
-			System.out.println("demo on webhooks - Jan 06 2021 demo4");
-			System.out.println("demo on webhooks - Jan 12 2021 demo52");
-			System.out.println("demo on webhooks - March 19 ");
-			System.out.println("demo on webhooks - March 19 second version");
-			System.out.println("demo on webhooks - March 03 april demo 44444");
-			System.out.println("demo on webhooks demo May 27 - demo1");
-			System.out.println("demo on webhooks demo July 01 - demo2 webhook");
-			System.out.println("demo on webhooks demo 12 Oct- demo11 webhook demo");
-			System.out.println("demo on webhooks demo 08 Dec javademo webhook demo");
-			System.out.println("demo on webhooks demo 29 Dec javademo webhook demo1");
+			System.out.println("Thanks for the webhooks demo2.. today");						
 		}
 	}
 	
