9b871b1a58b4f43ca7d24f6579674ec78b5133c0
salagarsprabu <salagars2019@gmail.com>
2022-08-06 12:41:36 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,11 +3,7 @@ public class Hello{
 	public static void main(String args[]){
 		for (int i=1;i<=10;i++){
 			System.out.println("Hello World....."+i);
-			System.out.println("Thanks for the webhooks demo..");
-			System.out.println("Thanks for the webhooks demo2..");			
-			System.out.println("Thanks for the webhooks demo3.. today 09-June-2022");
-			System.out.println("Thanks for the webhooks demo4.. June-2022");
-			System.out.println("Thanks for the webhooks demo4.. June 22-2022");
+			System.out.println("Thanks for the webhooks demo..");			
 			System.out.println("Thanks for the webhooks demos today - Aug 06-2022");
 		}
 	}
