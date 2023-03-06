96f67e68226fcd9f67af9859967880aa21ae0a9c
salagarsprabu <salagars2019@gmail.com>
2023-03-06 11:54:21 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ public class Hello{
 			System.out.println("Hello World....."+i);
 			System.out.println("Thanks for the webhooks demo..");			
 			System.out.println("Thanks for the webhooks demos today - Aug 06-2022");
-			System.out.println("Thanks for the webhooks demos today - Feb 15 -2023");
+			System.out.println("Thanks for the webhooks demos today ");
 			System.out.println("Thanks for the webhooks demos today - March 06 -2023");
 		}
 	}
